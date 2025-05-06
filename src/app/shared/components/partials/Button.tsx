e9ce3d0ba645367aad5a3ad01b243d4d169ccbd4
st-thule <thu.le@supremetech.vn>
2025-05-06 23:10:08 +0700

    feat: implement button component

---
+++ src/app/shared/components/partials/Button.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface IButtonProps {
+  className?: string;
+  type?: 'button' | 'submit';
+  label?: string | '';
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+export const Button: React.FC<IButtonProps> = ({
+  className = '',
+  type = 'button',
+  label,
+  onClick,
+}) => {
+  return (
+    <button className={`btn ${className}`} type={type} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
