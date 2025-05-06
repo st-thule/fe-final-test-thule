3f05c800fc64ddf75deb7ffe3862c81231005bd1
st-thule <thu.le@supremetech.vn>
2025-05-06 22:30:56 +0700

    feat: add constant list for footer

---
+++ src/app/shared/components/layouts/Footer/footer.ts
@@ -0,0 +1,19 @@
+import { href } from 'react-router-dom';
+
+export const footerList = [
+  {
+    id: 1,
+    title: 'Privacy Policy',
+    href: '',
+  },
+  {
+    id: 2,
+    title: 'Term of service',
+    href: '',
+  },
+  {
+    id: 3,
+    title: 'Cooking settings',
+    href: '',
+  },
+];
