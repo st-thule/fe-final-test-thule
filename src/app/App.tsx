8e7da61c5adef1b43e5331e0ba0537d348dc69a6
st-thule <thu.le@supremetech.vn>
2025-05-06 07:58:33 +0700

    feat: define basic route

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { BrowserRouter, useRoutes } from 'react-router-dom';
+
+import appRoutes from './app.routes';
+
+const App = () => {
+  const routes = useRoutes(appRoutes);
+  return routes;
+};
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
