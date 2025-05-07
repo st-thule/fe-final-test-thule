c6c89d1c410ad5702434ac4e10c36cfdaf8fbebd
st-thule <thu.le@supremetech.vn>
2025-05-07 17:14:22 +0700

    feat: define for process

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,54 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createHtmlPlugin } from 'vite-plugin-html';
-import path from 'path';
+import Path from 'path';
+
+const PATHS = {
+  output: Path.join(__dirname, './dist'),
+  source: Path.join(__dirname, './src'),
+  fixed: '/',
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: './src',
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      minify: true,
-      /**
-       * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
-       * @default /app/App.tsx
-       */
-      entry: '/app/App.tsx',
-      /**
-       * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
-       * @default index.html
-       */
-      template: 'index.html',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@app': path.resolve(__dirname, './src/app'),
-      '@config': path.resolve(__dirname, './src/config'),
-      '@stylesheet': path.resolve(__dirname, './src/app/stylesheet'),
-      '@shared': path.resolve(__dirname, './src/app/shared'),
-      '@core': path.resolve(__dirname, './src/app/core'),
-      '@assets': path.resolve(__dirname, './src/assets'),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    root: PATHS.source,
+    base: PATHS.fixed,
+    publicDir: 'assets',
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        /**
+         * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
+         * @default /app/App.tsx
+         */
+        entry: '/app/App.tsx',
+        /**
+         * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
+         * @default index.html
+         */
+        template: 'index.html',
+      }),
+    ],
+    define: {
+      'process.env.APP_ENV': JSON.stringify(process.env.VITE_APP_ENV),
+      'process.env.API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
     },
-  },
-});
+    resolve: {
+      alias: {
+        '@app': Path.resolve(__dirname, './src/app'),
+        '@config': Path.resolve(__dirname, './src/config'),
+        '@stylesheet': Path.resolve(__dirname, './src/stylesheet'),
+        '@shared': Path.resolve(__dirname, './src/app/shared'),
+        '@core': Path.resolve(__dirname, './src/app/core'),
+        '@assets': Path.resolve(__dirname, './src/assets'),
+      },
+    },
+    build: {
+      // Specify the dist folder
+      outDir: PATHS.output,
+    },
+  });
+};
