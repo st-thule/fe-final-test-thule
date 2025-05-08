ac910c08d0edc5439316fed14dd7ce9317e233f2
st-thule <thu.le@supremetech.vn>
2025-05-08 14:40:55 +0700

    refactor: delete redudant code

--- src/app/shared/components/layouts/Header.tsx
+++ src/app/shared/components/layouts/Header.tsx
@@ -2,15 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AppRoutes } from '@app/core/constants/app-routes';
-import { USER_KEY } from '@app/core/constants/contants';
 import { authStorage } from '@app/core/services/auth-storage.service';
 import { Button } from '../partials/Button';
-import { getDataFromLocalStorage } from '@app/core/helpers/storage.helper';
-import { User } from '@shared/models/user';
 
 export const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const user = getDataFromLocalStorage(USER_KEY, {});
 
   useEffect(() => {
     const token = authStorage.getToken();
