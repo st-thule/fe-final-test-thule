7eb3c7ab836cb0e7b64d870200b084ace0684b8a
st-thule <thu.le@supremetech.vn>
2025-05-09 14:15:36 +0700

    refactor: change order of import

--- src/app/pages/posts/containers/PostDetail.tsx
+++ src/app/pages/posts/containers/PostDetail.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import calendarIcon from '@assets/icons/calendar.svg';
-import { Link } from 'react-router-dom';
 
 const PostDetail = () => {
   return (
