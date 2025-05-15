0aad58bb77f3f96bff9bea9efa5bed8d70e6bc5a
st-thule <thu.le@supremetech.vn>
2025-05-15 16:46:56 +0700

    feat: style for layout my profile

--- src/app/pages/user/containers/Profile.tsx
+++ src/app/pages/user/containers/Profile.tsx
@@ -2,23 +2,34 @@ import React from 'react';
 
 import { Button } from '@shared/components/partials';
 
+import avatar from '@assets/icons/avatar.svg';
+
 const Profile = () => {
   return (
     <div className="page page-profile">
-      <section className="section section-info">
-        <img className="img avatar" src="" />
-        <div className="section-text">
-          <h1 className="section-title"></h1>
-          <p className="section-subtitle"></p>
+      <div className="container">
+        <div className="wrapper wrapper-padding">
+          <section className="section section-info">
+            <div className="section-image">
+              <img className="img avatar" src={avatar} />
+            </div>
+            <div className="section-content">
+              <div className="section-text">
+                <h1 className="section-title">Lê Anh Thư</h1>
+                <p className="section-subtitle">thule210903@gmail.com</p>
+              </div>
+              <Button label="Edit" className="btn btn-primary" />
+            </div>
+          </section>
+          <div className=""></div>
+          <section className="section section-list section-post">
+            <div className="section-header">
+              <h2 className="section-title">My Article</h2>
+              <ul className="list list-posts"></ul>
+            </div>
+          </section>
         </div>
-        <Button label="Edit" className="btn btn-primary" />
-      </section>
-      <section className="section section-list section-post">
-        <div className="section-header">
-          <h2 className="section-title">My Article</h2>
-          <ul className="list list-posts"></ul>
-        </div>
-      </section>
+      </div>
     </div>
   );
 };
