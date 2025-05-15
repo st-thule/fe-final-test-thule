c6799c078733a1d6cf6fb57d919060b4bfdcebdd
st-thule <thu.le@supremetech.vn>
2025-05-15 08:22:16 +0700

    feat: build layout for profile

--- src/app/pages/user/containers/Profile.tsx
+++ src/app/pages/user/containers/Profile.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import { Button } from '@shared/components/partials';
+
 const Profile = () => {
-  return <>This is profile page</>;
+  return (
+    <div className="page page-profile">
+      <section className="section section-info">
+        <img className="img avatar" src="" />
+        <div className="section-text">
+          <h1 className="section-title"></h1>
+          <p className="section-subtitle"></p>
+        </div>
+        <Button label="Edit" className="btn btn-primary" />
+      </section>
+      <section className="section section-list section-post">
+        <div className="section-header">
+          <h2 className="section-title">My Article</h2>
+          <ul className="list list-posts"></ul>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Profile;
