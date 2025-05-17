63b09a6c240a840364caaeef372e5003bf639681
st-thule <thu.le@supremetech.vn>
2025-05-17 09:00:16 +0700

    feat: define router for posts by tag

--- src/app/shared/components/TagComponent.tsx
+++ src/app/shared/components/TagComponent.tsx
@@ -1,14 +1,17 @@
+import { AppRoutes } from '@app/core/constants/app-routes';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 interface ITagProps {
   className?: string;
+  label: string;
 }
-export const Tag: React.FC<ITagProps> = ({ className }) => {
+
+export const Tag: React.FC<ITagProps> = ({ className, label }) => {
   return (
     <li className={`list-item ${className}`}>
-      <Link className="tag" to="">
-        <h3 className="tag-title">Technology</h3>
+      <Link className="tag" to={`${AppRoutes.POSTS}/tags/${label}`}>
+        <h3 className="tag-title">{label}</h3>
       </Link>
     </li>
   );
