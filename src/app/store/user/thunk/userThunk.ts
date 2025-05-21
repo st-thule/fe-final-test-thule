0a6a79e5c628dbb486abda2a0e0b9542326e023a
st-thule <thu.le@supremetech.vn>
2025-05-21 22:27:53 +0700

    feat: implement redux for update info

--- src/app/store/user/thunk/userThunk.ts
+++ src/app/store/user/thunk/userThunk.ts
@@ -1,7 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import { UserWithPost } from '@shared/models/user';
-import { UserService } from '@shared/services/user.service';
+import { User, UserWithPost } from '@shared/models/user';
+import { IUserPayload, UserService } from '@shared/services/user.service';
+import { data } from 'react-router-dom';
 
 const userService = new UserService();
 
@@ -22,5 +23,18 @@ export const getPersonalInfoThunk = createAsyncThunk<
     };
   } catch (error) {
     return rejectWithValue(error.message || 'Failed to fetch user info');
+  }
+});
+
+export const updateInfoThunk = createAsyncThunk<
+  User,
+  IUserPayload,
+  { rejectValue: string }
+>('user/updateInfo', async (data, { rejectWithValue }) => {
+  try {
+    const updatedUser = await userService.updateProfile(data);
+    return updatedUser;
+  } catch (error) {
+    return rejectWithValue(error.message || 'Failed to update info');
   }
 });
