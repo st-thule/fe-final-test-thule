06c33ab58d8d8d563e37ffbc6ca74100141ad4dd
st-thule <thu.le@supremetech.vn>
2025-05-22 23:24:29 +0700

    refactor: change location show of toast

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -43,6 +43,6 @@ createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-    <ToastContainer position="top-right" autoClose={3000} />
+    <ToastContainer position="top-center" autoClose={3000} />
   </>
 );
