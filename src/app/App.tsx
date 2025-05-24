76cb6ab8b644a2397c8c5901f43ad26351b5df89
st-thule <thu.le@supremetech.vn>
2025-05-25 00:25:04 +0700

    feat: change run time of toast

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -43,6 +43,6 @@ createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-    <ToastContainer position="top-center" autoClose={3000} />
+    <ToastContainer position="top-center" autoClose={1000} />
   </>
 );
