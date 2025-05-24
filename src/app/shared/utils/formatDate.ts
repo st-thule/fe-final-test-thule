b0529db6f73820822f91008d8d4a707314de2355
st-thule <thu.le@supremetech.vn>
2025-05-25 00:33:12 +0700

    feat: add case dateString is valid

--- src/app/shared/utils/formatDate.ts
+++ src/app/shared/utils/formatDate.ts
@@ -1,5 +1,10 @@
 export const formatDate = (dateString: string): string => {
   if (!dateString) throw new Error('Invalid date');
+
+  const ddMmYyyyPattern = /^\d{2}\/\d{2}\/\d{4}$/;
+  if (ddMmYyyyPattern.test(dateString)) {
+    return dateString;
+  }
   const date = new Date(dateString);
   if (isNaN(date.getTime())) throw new Error('Invalid date');
   const day = date.getDate().toString().padStart(2, '0');
